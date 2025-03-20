793764a73592e60ae0d18484d473f317bcbdf57a
Irving Salazar <ISCIrvingDev@gmail.com>
2025-03-20 17:02:39 -0600

    Init Project

---
+++ products.py
@@ -0,0 +1,17 @@
+products = [
+  {
+    "name": "Laptop",
+    "price": 800,
+    "quantity": 4
+  },
+  {
+    "name": "Xbox",
+    "price": 500,
+    "quantity": 8
+  },
+  {
+    "name": "IPhone",
+    "price": 1500,
+    "quantity": 10
+  },
+]
